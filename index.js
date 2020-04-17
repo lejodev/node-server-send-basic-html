30673d2eb3143ce756b4043903e4b341a6bdde08
Alejandro Rincón Vera <alejandro.rv97@gmail.com>
2020-04-17 17:53:20 -0500

    serving static files added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/assets',express.static('assets')); // Static files (should be separated from the server code)
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {
